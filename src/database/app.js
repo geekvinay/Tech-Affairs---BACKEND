90d681dc9e5b874128fcda1ef75696d05c271664
geekvinay <geekvinay002@gmail.com>
2022-10-31 21:07:59 +0530

    Added online database

--- src/database/app.js
+++ src/database/app.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/techaffairs-backend', {
-  useNewUrlParser: true,
-});
+mongoose.connect(
+  'mongodb+srv://geekvinay002:<password>@cluster0.gkwn94o.mongodb.net/?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+  }
+);
